376e4e823b89a1dfd2a46e70760403ac0833cb0c
Esther Lee <54889800+14youlim@users.noreply.github.com>
2019-10-10 11:31:57 +0900

    Add files via upload

---
+++ CH3-각종 함수.py
@@ -0,0 +1,41 @@
+#계단 함수
+import numpy as np
+import matplotlib.pylab as plt
+
+
+def step_function(x):
+    return np.array(x > 0, dtype=np.int)
+
+X = np.arange(-5.0, 5.0, 0.1)
+Y = step_function(X)
+plt.plot(X, Y)
+plt.ylim(-0.1, 1.1)  # y축의 범위 지정
+plt.show()
+
+#시그모이드 함수
+import numpy as np
+import matplotlib.pylab as plt
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))    
+
+X = np.arange(-5.0, 5.0, 0.1)
+Y = sigmoid(X)
+plt.plot(X, Y)
+plt.ylim(-0.1, 1.1)
+plt.show()
+
+#ReLU 함수
+import numpy as np
+import matplotlib.pylab as plt
+
+
+def relu(x):
+    return np.maximum(0, x)
+
+x = np.arange(-5.0, 5.0, 0.1)
+y = relu(x)
+plt.plot(x, y)
+plt.ylim(-1.0, 5.5)
+plt.show()
